629dc23cffd826d547419da640048456094db6de
Glenn <glenn.ferrie@gmail.com>
2024-04-02 00:55:29 -0400

    moves some properties to the base class.

--- api/Common/Payload/ResponseBodyBase.cs
+++ api/Common/Payload/ResponseBodyBase.cs
@@ -1,4 +1,11 @@
-﻿namespace infura.web3.Common
+﻿using System.Text.Json.Serialization;
+
+namespace infura.web3.Common
 {
-    public abstract class ResponseBodyBase { }
+    public abstract class ResponseBodyBase {
+        [JsonPropertyName("jsonrpc")]
+        public string JsonRPCVersion { get; set; } = "2.0";
+        [JsonPropertyName("id")]
+        public int Id { get; set; } = 1;
+    }
 }
--- api/Ethereum/Payload/GetChainIdResponseBody.cs
+++ api/Ethereum/Payload/GetChainIdResponseBody.cs
@@ -5,11 +5,7 @@ namespace bc.infura.web3.Ethereum
 {
     public class GetChainIdResponseBody : ResponseBodyBase
     {
-        [JsonPropertyName("jsonrpc")]
-        public string JsonRPCVersion { get; set; } = "2.0";
         [JsonPropertyName("result")]
         public string Result { get; set; } = "0x0";
-        [JsonPropertyName("id")]
-        public int Id { get; set; } = 1;
     }
 }
